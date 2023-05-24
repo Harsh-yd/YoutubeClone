8a304cf4e86b3fdc0b2607aa1a86887428e48ac7
Harsh-yd <132829664+Harsh-yd@users.noreply.github.com>
2023-05-24 17:51:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+
+
+
+document.getElementsByClassName("y1")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=n2RNcPRtAiY&pp=ygUyVGFsa2luZyBUZWNoIGFuZCBBSSB3aXRoIEdvb2dsZSBDRU8gU3VuZGFyIFBpY2hhaSE%3D");
+}
+)
+document.getElementsByClassName("y2")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=mP0RAo9SKZk&pp=ygUqIFRyeSBOb3QgVG8gTGF1Z2ggQ2hhbGxlbmdlICM5ICBNYXJraXBsaWVy");
+}
+)
+document.getElementsByClassName("y3")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=FgjPQQeTh1w&pp=ygU6Q3JhenkgVGlrIFRva3MgVGFrZW4gTW9tZW50cyBCZWZvcmUgRElTQVNURVIgIFNTU25pcGVyV29sZg%3D%3D");
+}
+)
+document.getElementsByClassName("y4")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=094y1Z2wpJg&pp=ygU_VGhlIFNpbXBsZXN0IE1hdGggUHJvYmxlbSBObyBPbmUgQ2FuIFNvbHZlIC0gQ29sbGF0eiBDb25qZWN0dXJl");
+}
+)
+document.getElementsByClassName("y5")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=86CQq3pKSUw&pp=ygU7S2FkYW5lJ3MgQWxnb3JpdGhtIHRvIE1heGltdW0gU3VtIFN1YmFycmF5IFByb2JsZW0gIENTIERvam8%3D");
+}
+)
+document.getElementsByClassName("y6")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=yXWw0_UfSFg&pp=ygU6QW55dGhpbmcgWW91IENhbiBGaXQgSW4gVGhlIENpcmNsZSBJ4oCZbGwgUGF5IEZvciAgTXJCZWFzdA%3D%3D");
+}
+)
+document.getElementsByClassName("y7")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=HFjxUV9tIH0&t=244s");
+}
+)
+document.getElementsByClassName("y8")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=cRlYOHIwUGw");
+}
+)
+document.getElementsByClassName("y9")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=Si6NbKqYEd8");
+}
+)
+document.getElementsByClassName("y10")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=u35WIs62R2M");
+}
+)
+document.getElementsByClassName("y11")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=R6t0f1_vwno");
+}
+)
+document.getElementsByClassName("y12")[0].addEventListener("click", function () {
+    window.open("https://www.youtube.com/watch?v=ljwHJP2rLrU");
+}
+)
